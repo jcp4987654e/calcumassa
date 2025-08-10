1e96215adb0ca0ca6242ef488ff116b7115e74bd
jcp4987654e <nlose38@gmail.com>
2025-08-10 16:17:28 -0300

    Delete calculadora_pro.py

--- calculadora_pro.py
+++
@@ -1,9 +0,0 @@
-
-import streamlit as st
-from pathlib import Path
-import streamlit.components.v1 as components
-
-st.set_page_config(page_title="Calculadoras Eléctricas PRO (AR)", layout="wide")
-html_file = "index_pro.html"
-src = Path(__file__).with_name(html_file).read_text(encoding="utf-8")
-components.html(src, height=1250, scrolling=True)
